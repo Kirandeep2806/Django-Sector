78138fbad2170cb3a02307a96aaa25d1331ed3b3
Kiran Deep <kirandeep102030@gmail.com>
2022-08-07 18:52:42 +0530

    Updated gitignore

--- DRFv1/DRFv1/urls.py
+++ DRFv1/DRFv1/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('EventKeeper.urls'), name=''),
+    path('', include('EventKeeper.urls'), name='event-keeper'),
 ]
