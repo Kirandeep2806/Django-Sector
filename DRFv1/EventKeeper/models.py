23c8d8be70055a4b72dc54672ce6124f22391159
Kiran Deep <kirandeep102030@gmail.com>
2022-08-12 22:11:22 +0530

    models@staging

--- DRFv1/EventKeeper/models.py
+++ DRFv1/EventKeeper/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class EventData(models.Model):
+    content = models.CharField(max_length=1000, default="")
+    date = models.DateTimeField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.content[:len(self.content) if len(self.content)<15 else 15]
