09d2c300d8cb9f80b61275370b9c17d463789964
Kiran Deep <kirandeep102030@gmail.com>
2022-10-27 02:45:45 +0530

    Updated the urls file

--- Django-Auth/DjangoAuth/AuthApp/urls.py
+++ Django-Auth/DjangoAuth/AuthApp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage, name="home-page"),
+    path('/login', views.UserLogin, name="login-page"),
+    path('/logout', views.UserLogout, name="logout-page"),
+    path('/register', views.UserRegister, name="register-page"),
 ]
--- Django-Auth/DjangoAuth/AuthApp/views.py
+++ Django-Auth/DjangoAuth/AuthApp/views.py
@@ -1,8 +1,15 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
+@login_required("/login")
 def HomePage(request):
     return render(request, "home.html")
 
+def UserLogin(request):
+    pass
 
+def UserLogout(request):
+    pass
+
+def UserRegister(request):
+    pass
